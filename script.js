c2cfc97026a14c894eaf8ab12a2e465235d9a5ff
mahdislo <mahdis.lohraseby@yahoo.com>
2021-01-29 16:55:06 +0100

    PriceChange-And-FlyoutMenu
    implementation of the price change based on the radio buttons and the flyout menu on the left side of the page

---
+++ script.js
@@ -0,0 +1,24 @@
+ function priceCalc() { 
+                    
+                var currentPrice = document.getElementById("current-price");
+                var previousPrice = document.getElementById("previous-price");
+                var sizes = document.getElementsByName("size");
+                    
+                for (var i=0; i< sizes.length; i++){
+                    if(sizes[i].checked) {
+                        if(sizes[i].value == 300) {
+                            currentPrice.textContent = "2,80 €";
+                            previousPrice.innerHTML = "<del>3,20 €</del>";
+                        }
+                        else if(sizes[i].value == 500) {
+                            currentPrice.textContent = "3,50 €";
+                            previousPrice.innerHTML = "<del>4,10 €</del>";
+                            
+                        }else if(sizes[i].value == 250) {
+                            currentPrice.textContent = "2,10 €";
+                            previousPrice.innerHTML = "<del>2,60 €</del>";
+                        }
+                    }
+                }   
+            }
+                  
